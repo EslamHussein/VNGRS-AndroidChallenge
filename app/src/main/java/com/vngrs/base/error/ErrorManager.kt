ec9402c4e090edd867b21faf6e6e2bc2efbc33fc
Eslam Hussein <eng.eslam.hussein1@gmail.com>
2018-06-16 20:13:08 +0200

    add exception Mapper

---
+++ app/src/main/java/com/vngrs/base/error/ErrorManager.kt
@@ -0,0 +1,25 @@
+package com.vngrs.base.error
+
+import io.reactivex.Observable
+import io.reactivex.functions.Function
+
+
+object ErrorManager {
+
+    fun <T> wrap(observable: Observable<T>): Observable<T> {
+        return observable.onErrorResumeNext(ExceptionsInterceptor())
+    }
+
+    /**
+     * Maps java exceptions to the appropriate [AppException]
+     */
+    private class ExceptionsInterceptor<T> : Function<Throwable, Observable<T>> {
+
+
+        @Throws(Exception::class)
+        override fun apply(throwable: Throwable): Observable<T> {
+            return Observable.error(AppException.adapt(throwable))
+        }
+    }
+
+}/* No instances */
